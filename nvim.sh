5ee8f2b27fae564ce7627961b7718d6e521893ef
Zerokei <twentyeightkei@gmail.com>
2022-07-24 09:55:50 +0800

    [FEATURE] move the cursor

--- nvim.sh
+++ nvim.sh
@@ -1,32 +1,73 @@
 #!/bin/bash
 input=$1
 echo $input
-lines=$(wc -l $input | awk '{print $1;}')
 
 # store the text
+lines=$(wc -l $input | awk '{print $1;}')
 arrVar=()
 while read -r line
 do
 	arrVar+=("$line")
 done < "$input"
+
+
 # output the text
-
-posc=2
-posl=1
-
+echo "wow"
 print(){
 	clear
-	echo "The text has "$lines" lines"
 	for index in $(seq 0 $((lines-1))) ; do
 		value=${arrVar[$index]}
-		if [ $posc -eq $index ]
+		if [ $(($1)) -eq $index ]
 		then
-			echo -e ${value:0:$((posl))}"\033[5m${value:$posl:1}\033[m"${value:1}
+            if [ $(($2)) -eq ${#value} ]
+            then
+                echo -e $value"\033[5m_\033[m"
+            else 
+			    echo -e ${value:0:$2}"\033[1;30m${value:$2:1}\033[m"${value:$(($2+1))}
+            fi
 		else
 			echo $value
 		fi
 		# echo $value
 	done
-	echo "-- VIEW --"
+	echo -en "-- VIEW --"
 }
+
+posc=0
+posl=0
 print
+# main program
+while true
+do
+    read -s -n1 acc
+    echo $acc
+    if [[ $acc = "l" ]]
+    then
+        posl=$posl+1
+    elif [[ $acc = "h" ]]
+    then
+        posl=$posl-1
+    elif [[ $acc = "j" ]]
+    then
+        posc=$posc+1
+    elif [[ $acc = "k" ]]
+    then
+        posc=$posc-1
+    fi
+    if [[ "$posc" -lt 0 ]]
+    then
+        posc=0
+    elif [[ "$posc" -ge "$lines" ]]
+    then
+        posc=$((lines))-1
+    fi
+    if [[ "$posl" -lt 0 ]]
+    then 
+        posl=0
+    elif [[ "$posl" -gt ${#arrVar[$posc]} ]]
+    then
+        echo "!"
+        posl=${#arrVar[$posc]}
+    fi
+    print posc posl
+done
---
+++ wow.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Demonstrate how read actually works
+echo What cars do you like?
+read car1 car2 car3
+echo Your first car was: $car1
+echo Your second car was: $car2
+echo Your third car was: $car3
